1329d96f0018f4f35badd03f418f87a8bc2af0a4
William Hoang <william@Williams-MacBook-Pro.local>
2017-03-02 02:56:14 -0600

    First Init

---
+++ CalculatorProject/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  CalculatorProject
+//
+//  Created by William Hoang on 3/2/17.
+//  Copyright © 2017 COSC 1430. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    string input, first;
+    int i =0;
+    int j = 0;
+    while(i==0)
+    {
+        cin >> input;
+        for(j; j < input.size(); j++)
+        {
+            if(isdigit(input[j]))
+            {
+                first = push_back(
+            }
+            
+        }
+
+    }
+
+    
+    return 0;
+}
+
